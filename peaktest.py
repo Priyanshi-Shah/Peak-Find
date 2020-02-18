c9f537c4bcffc2534ebb338b5b3841e83042dc94
ps <priyanshi184@gmail.com>
2020-02-18 16:51:03 +0530

    Add- Checked For 2 elemnts in list

--- peaktest.py
+++ peaktest.py
@@ -4,7 +4,9 @@ import unittest
 def get_peak(arr):
     if not arr:
         return None
-    if arr[0] == arr[0]:
+    if len(arr)>1:
+        return max(arr)
+    else:
         return arr[0]
 
 
@@ -18,6 +20,11 @@ class PeakTest(unittest.TestCase):
 
     def test_check_for_two(self):
         self.assertEqual(2, get_peak([2]))
+
+    def test_check_for_two_elements(self):
+        self.assertEqual(2, get_peak([1,2]))
+
+
 
 
 if __name__ == '__main__':

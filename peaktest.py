9a7b55e9a35d8df63cb3cbfe8c486f97425139a5
ps <priyanshi184@gmail.com>
2020-02-18 17:08:46 +0530

    Add- Peak found if peak is at second poistion in 3 elements

--- peaktest.py
+++ peaktest.py
@@ -3,11 +3,11 @@ import unittest
 
 def get_peak(arr):
     p = 0
-    #i = 0
+    i = 0
     if not arr:
         return None
     if len(arr) > 1:
-        if arr[1] > arr[0]:
+        if arr[i+1] > arr[i]:
             p=1
         return arr[p]
     else:
@@ -27,6 +27,9 @@ class PeakTest(unittest.TestCase):
 
     def test_check_for_two_elements(self):
         self.assertEqual(2, get_peak([1, 2]))
+
+    def test_check_for_three_elements(self):
+        self.assertEqual(3, get_peak([1, 3, 2]))
 
 
 
